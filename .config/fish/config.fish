e819453ce56ab67c9ba4b3753d50e6ea7a96118a
Manu Wallner <wallman@amazon.com>
2019-08-02 15:34:32 -0700

    Add dependencies

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,9 @@
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
+
 # Make nvim default editor
 set -gx EDITOR nvim
 set -gx GIT_EDITOR nvim
@@ -19,4 +25,10 @@ set -g fish_user_paths "/usr/local/opt/llvm/bin" $fish_user_paths
 set -g theme_color_scheme 'dracula'
 set -g theme_date_format "+%H:%M:%S"
 
+status --is-interactive; and source (pyenv init -|psub)
+
 test -e ~/.config/fish/local.fish; and source ~/.config/fish/local.fish
+
+source ~/.config/fish/functions/dracula.fish
+
+alias cp='rsync --info=progress2'
