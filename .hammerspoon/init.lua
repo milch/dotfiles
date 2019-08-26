cf1b686d6eba3164beda44d121d340fa8364172a
Manu Wallner <wallman@amazon.com>
2019-08-26 15:03:47 -0700

    Refactor hammerspoon script, better support for windows that can't be arbitrarily resized (iTerm)

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,7 +1,34 @@
 local hyper = {"ctrl", "alt", "cmd"}
 
+-- Helper functions
 function round(x)
   return x + 0.5 - (x + 0.5) % 1
+end
+
+function compareFrame(lhs, rhs)
+  local deltaX = lhs.x - rhs.x
+  local deltaY = lhs.y - rhs.y
+  local deltaH = lhs.h - rhs.h
+  local deltaW = lhs.w - rhs.w
+
+  local rms = (deltaX^2 + deltaY^2 + deltaH^2 + deltaW^2)^0.5
+  return rms <= 5.0
+end
+
+function map(func, array)
+  local new_array = {}
+  for idx, value in ipairs(array) do
+    new_array[idx] = func(value)
+  end
+  return new_array
+end
+
+function range(num, start)
+  local arr = {}
+  for i=start,num,1 do
+    arr[#arr+1] = i
+  end
+  return arr
 end
 
 -- Auto reload config on changes
@@ -19,95 +46,63 @@ end
 myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
 hs.alert.show("Config loaded")
 
--- Window movement - left half, right, half, etc.
-hs.hotkey.bind(hyper, "Right", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
+function frames()
+  local screenFrame = hs.screen.mainScreen():frame()
+  local supportedFractionCount = 8
 
-  f.h = max.h
-  f.y = max.y
+  local middleThird = hs.screen.mainScreen():frame()
+  middleThird.x = screenFrame.x + (screenFrame.w / 3)
+  middleThird.w = screenFrame.w / 3
 
-  local current_fraction = nil
-  for fraction=2,8,1 do
-      f.w = round(max.w / fraction)
-      f.x = round(max.x + (max.w / fraction * (fraction - 1)))
-      if win:frame() == f then
-          current_fraction = fraction
-          break
-      end
+  local frames = {
+    ["Right"] = map(
+        function(f)
+          frame = hs.screen.mainScreen():frame()
+          frame.x = round(screenFrame.x + (screenFrame.w / f * (f - 1)))
+          frame.w = round(screenFrame.w / f)
+          return frame
+        end,
+      range(supportedFractionCount, 2)),
+    ["Left"] = map(
+        function(f)
+          frame = hs.screen.mainScreen():frame()
+          frame.w = round(screenFrame.w / f)
+          return frame
+        end,
+      range(supportedFractionCount, 2)),
+    ["Up"] = { screenFrame },
+    ["Down"] = { middleThird }
+  }
+
+  return frames
+end
+
+function findNextFrame(currentFrame, type)
+  local framesOfType = frames()[type]
+  for idx, frame in pairs(framesOfType) do
+    if compareFrame(frame, currentFrame) then
+      return framesOfType[idx + 1]
+    end
   end
 
-  if current_fraction then
-      current_fraction = current_fraction + 1
-  else
-      current_fraction = 2
-  end
+  -- Default to the first available frame
+  return framesOfType[1]
+end
 
-  f.x = max.x + round(max.w / current_fraction * (current_fraction - 1))
-  f.w = round(max.w / current_fraction)
+function windowMovement(keys, direction)
+  hs.hotkey.bind(keys, direction, function()
+    local win = hs.window.focusedWindow()
 
-  win:setFrame(f)
-end)
+    local foundFrame = findNextFrame(win:frame(), direction)
 
-hs.hotkey.bind(hyper, "Left", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
+    win:setFrame(foundFrame)
+  end)
+end
 
-  f.x = max.x
-  f.y = max.y
-  f.h = max.h
-
-  local current_fraction = nil
-  for fraction=2,8,1 do
-      f.w = round(max.w / fraction)
-      if win:frame() == f then
-          current_fraction = fraction
-          break
-      end
-  end
-
-  if current_fraction then
-      current_fraction = current_fraction + 1
-  else
-      current_fraction = 2
-  end
-
-  f.w = round(max.w / current_fraction)
-
-  win:setFrame(f)
-end)
-
-hs.hotkey.bind(hyper, "Up", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-
-  f.x = max.x
-  f.y = max.y
-  f.w = max.w
-  f.h = max.h
-
-  win:setFrame(f)
-end)
-
-hs.hotkey.bind(hyper, "Down", function()
-  local win = hs.window.focusedWindow()
-  local f = win:frame()
-  local screen = win:screen()
-  local max = screen:frame()
-
-  f.x = max.x + (max.w / 3)
-  f.y = max.y
-  f.w = max.w / 3
-  f.h = max.h
-
-  win:setFrame(f)
-end)
+windowMovement(hyper, "Right")
+windowMovement(hyper, "Left")
+windowMovement(hyper, "Up")
+windowMovement(hyper, "Down")
 
 -- Move window to a different screen
 hs.hotkey.bind(hyper, "Tab", function()
