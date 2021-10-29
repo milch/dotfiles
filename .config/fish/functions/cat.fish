842463c8b491cd2f815fd9d190664ea1e9e1a138
Manu Wallner <manu@supermil.ch>
2021-10-29 15:45:32 -0700

    Choose bat dark theme when dark mode is enabled

--- .config/fish/functions/cat.fish
+++ .config/fish/functions/cat.fish
@@ -1,4 +1,9 @@
-# Defined in - @ line 0
+# Defined in /var/folders/hl/swx6fly14j50lcyd5_rq2mlh0000gn/T//fish.t5mbbb/cat.fish @ line 2
 function cat --description 'alias cat=bat'
-	bat --theme=GitHub $argv;
+	set theme GitHub
+	if [ "$APPLE_INTERFACE_STYLE" = "dark" ]
+		set theme Dracula
+	end
+
+	bat --theme=$theme $argv;
 end
