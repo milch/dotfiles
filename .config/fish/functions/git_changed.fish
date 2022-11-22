370e9182f5da5961cea013e33539346d65223e12
Manu Wallner <wallman@amazon.com>
2022-11-22 10:43:57 -0800

    Use basename for changed git files

--- .config/fish/functions/git_changed.fish
+++ .config/fish/functions/git_changed.fish
@@ -2,7 +2,7 @@ function git_changed
 	set dirs (fd --type directory --maxdepth 1)
 	for dir in $dirs
 		if git -C $dir status | rg 'ahead' >/dev/null
-			set changed $changed $dir
+			set changed $changed (basename $dir)
 		end
 	end
 	echo $changed
