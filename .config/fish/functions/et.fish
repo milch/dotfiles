e474eeb33540dc40e302fb0a9d74e5488d6c3b81
Manu Wallner <wallman@amazon.com>
2022-11-22 10:43:42 -0800

    Update paths for M1

--- .config/fish/functions/et.fish
+++ .config/fish/functions/et.fish
@@ -1,9 +1,9 @@
 # Defined in /var/folders/zn/vphczcxn01g4lg4tnf8w0_k1yqft1l/T//fish.7LTSRa/et.fish @ line 2
 function et
 	if string match -q "*dev-desktop*" "$argv[-1]"
-		/usr/local/bin/et -c 'cd '(pwd | ruby -e "puts ARGF.readline.chomp.sub(%r(^$HOME), '~')")' && printf "\033c" && zsh' $argv
+		/opt/homebrew/bin/et -c 'cd '(pwd | ruby -e "puts ARGF.readline.chomp.sub(%r(^$HOME), '~')")' && printf "\033c" && zsh' $argv
 	else
 		# Not dev desktop, just pass through
-		/usr/local/bin/et $argv
+		/opt/homebrew/bin/et $argv
 	end
 end
