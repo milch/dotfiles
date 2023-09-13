839b8c3c5c7aad3b024ae8fff729d1033210e2dd
Manu Wallner <manu@supermil.ch>
2023-09-13 11:10:54 -0700

    Use lsp-zero built-in nvim-cmp action helpers

--- .config/nvim/lua/completion/nvim-cmp.lua
+++ .config/nvim/lua/completion/nvim-cmp.lua
@@ -1,11 +1,5 @@
-local has_words_before = function()
-	unpack = unpack or table.unpack
-	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
-local luasnip = require("luasnip")
 local cmp = require("cmp")
+local cmp_action = require("lsp-zero").cmp_action()
 
 local cmp_kinds = {
 	Text = "",
@@ -49,10 +43,7 @@ cmp.setup({
 			require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
 		end,
 	},
-	window = {
-		-- completion = cmp.config.window.bordered(),
-		-- documentation = cmp.config.window.bordered(),
-	},
+	window = {},
 	mapping = cmp.mapping.preset.insert({
 		["<C-b>"] = cmp.mapping.scroll_docs(-4),
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
@@ -69,33 +60,12 @@ cmp.setup({
 			s = cmp.mapping.confirm({ select = true }),
 			c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
 		}),
-		["<Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			elseif luasnip.expand_or_locally_jumpable() then
-				luasnip.expand_or_jump()
-			elseif has_words_before() then
-				cmp.complete()
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
-
-		["<S-Tab>"] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			elseif luasnip.jumpable(-1) then
-				luasnip.jump(-1)
-			else
-				fallback()
-			end
-		end, { "i", "s" }),
+		["<Tab>"] = cmp_action.luasnip_next(),
+		["<S-Tab>"] = cmp_action.luasnip_shift_supertab(),
 	}),
 	sources = cmp.config.sources({
 		{ name = "nvim_lsp" },
 		{ name = "luasnip" }, -- For luasnip users.
 		{ name = "path" },
-	}, {
-		{ name = "buffer" },
 	}),
 })
