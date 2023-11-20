781f5808ca5f6029c542f1e653924ce1897f815e
Manu Wallner <wallman@amazon.com>
2023-11-20 11:47:09 -0800

    Fix `gpm` whitespace issue

--- .config/fish/functions/gpm.fish
+++ .config/fish/functions/gpm.fish
@@ -1,5 +1,5 @@
 # Defined in /var/folders/hl/swx6fly14j50lcyd5_rq2mlh0000gn/T//fish.ilbfch/gpm.fish @ line 2
 function gpm --description 'alias gpm=git pull --rebase origin mainline --autostash'
-	set main_branch (git branch --color=never --list "main" "mainline" "master" | sed 's/\* //')
+	set main_branch (git branch --color=never --list "main" "mainline" "master" | sed -E 's/\*? *//g')
 	git pull --rebase origin $main_branch --autostash $argv;
 end
