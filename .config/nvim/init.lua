452e7c205568439642c93cd159c3d3652a05e56f
Manu Wallner <manu@supermil.ch>
2023-12-23 11:31:15 -0800

    Refactor global options into different files

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,21 +1,4 @@
-vim.g.loaded_gzip = 1
-vim.g.loaded_tar = 1
-vim.g.loaded_tarPlugin = 1
-vim.g.loaded_zip = 1
-vim.g.loaded_zipPlugin = 1
-vim.g.loaded_getscript = 1
-vim.g.loaded_getscriptPlugin = 1
-vim.g.loaded_vimball = 1
-vim.g.loaded_vimballPlugin = 1
-vim.g.loaded_matchit = 1
-vim.g.loaded_matchparen = 1
-vim.g.loaded_2html_plugin = 1
-vim.g.loaded_logiPat = 1
-vim.g.loaded_rrhelper = 1
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-vim.g.loaded_netrwSettings = 1
-vim.g.loaded_netrwFileHandlers = 1
+require("disable_defaults")
 
 -- Leader -> to prefix your own keybindings
 vim.g.mapleader = ","
@@ -34,87 +17,24 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins")
+require("lazy").setup({
+	import = "plugins",
+	change_detection = {
+		notify = false,
+	},
+})
 require("ui.use_system_theme").UpdateWhenSystemChanges()
 pcall(require, "init_local")
+require("opt")
+require("keybindings")
 
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
-
--- Smart indentation of lines
-vim.opt.smartindent = true
-
--- Highlight matching brackets
-vim.opt.showmatch = true
-
--- Ignore case when searching, but not if keywords are capitalized
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-
--- Turn off backups
-vim.opt.backup = false
-vim.opt.writebackup = true
-vim.opt.swapfile = true
-
--- No Arrow keys
-vim.api.nvim_set_keymap("", "<Left>", "<Nop>", {})
-vim.api.nvim_set_keymap("", "<Right>", "<Nop>", {})
-vim.api.nvim_set_keymap("", "<Up>", "<Nop>", {})
-vim.api.nvim_set_keymap("", "<Down>", "<Nop>", {})
-
--- Viewport will move if Cursor is 10 lines away from the edge
-vim.opt.scrolloff = 10
-
-vim.opt.relativenumber = true -- relative line numbers
-vim.opt.number = true         -- But on the current line, show the absolute line number
-
-vim.opt.hidden = true         -- Allow hidden buffers
-
-vim.opt.completeopt = "menu,menuone,noinsert,noselect"
-
--- always show signcolumns
-vim.opt.signcolumn = "yes"
-
--- Make Y behave like D
-vim.api.nvim_set_keymap("n", "Y", "y$", { noremap = true })
-
-vim.api.nvim_create_augroup("Spelling", {})
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = { "markdown", "mkd", "text", "plaintex", "tex" },
-	command = "set spell spelllang=en",
-	group = "Spelling",
-})
-
--- Working with buffers
-vim.api.nvim_set_keymap("n", "<Tab>", ":bn<CR>", { silent = true })
-vim.api.nvim_set_keymap("n", "<S-Tab>", ":bp<CR>", { silent = true })
-vim.api.nvim_set_keymap("n", "<leader>q", ":bd<CR>", { silent = true })
-
-vim.api.nvim_create_augroup("fixAutoread", {})
-vim.api.nvim_create_autocmd("FocusGained", {
-	command = "silent! checktime",
-	group = "fixAutoread",
-})
-
-vim.opt.mouse = "a"
-table.insert(vim.opt.shortmess, "c")
-vim.opt.listchars = "tab:>·,trail:~,extends:>,precedes:<"
-vim.opt.list = true
-
-vim.opt.shada = "'1,/10,<50,s10,h"
-vim.api.nvim_create_augroup("shareData", {})
-vim.api.nvim_create_autocmd("FocusGained", { command = "rshada" })
-vim.api.nvim_create_autocmd("TextYankPost", { command = "wshada" })
-
-vim.api.nvim_create_autocmd("BufEnter", {
-	callback = function(args)
-		local max_size = 1024 * 1024
-		if vim.fn.getfsize(args.file) > max_size then
-			vim.cmd([[
-            syntax off
-            set inccommand=
-            ]])
-		end
+-- Blink the thing that you just yanked
+vim.api.nvim_create_autocmd("TextYankPost", {
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank({
+			higroup = "IncSearch",
+			timeout = 100,
+		})
 	end,
 })
---
+++ .config/nvim/lua/keybindings.lua
@@ -0,0 +1,27 @@
+local bind = vim.keymap.set
+
+-- Make Y behave like D
+bind("n", "Y", "y$", { noremap = true, desc = "Make Y behave like D" })
+
+-- Working with buffers
+bind("n", "<Tab>", ":bn<CR>", { silent = true, desc = "Open next buffer" })
+bind("n", "<S-Tab>", ":bp<CR>", { silent = true, desc = "Open previous buffer" })
+bind("n", "<leader>q", ":bd<CR>", { silent = true, desc = "Close current buffer" })
+
+bind("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selected lines up" })
+bind("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selecetd lines down" })
+
+bind("n", "J", "mzJ`zmz", { desc = "Join lines (keep cursor position the same)" })
+bind("n", "<C-d>", "<C-d>zz", { desc = "Move one page down (keep cursor centered)" })
+bind("n", "<C-u>", "<C-u>zz", { desc = "Move one page up (keep cursor centered)" })
+
+bind("x", "<C-p>", [["_dP]], { desc = "Replace the selection with the paste buffer, preserve the buffer" })
+bind({ "n", "v" }, "<leader>y", [["+y]], { desc = "Copy to system clipboard" })
+bind("n", "<leader>Y", [["+Y]], { desc = "Copy to system clipboard" })
+
+bind(
+	"n",
+	"<leader>s",
+	[[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]],
+	{ desc = "Write :%s for the word under the cursor" }
+)
