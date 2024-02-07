6ff738e6d8712edf736a434cccb6a4b16fd959e4
Manu Wallner <wallman@amazon.com>
2024-02-06 16:35:07 -0800

    Add file name param to fssh

--- .config/fish/functions/fssh.fish
+++ .config/fish/functions/fssh.fish
@@ -1,4 +1,8 @@
 # Defined in /var/folders/zn/vphczcxn01g4lg4tnf8w0_k1yqft1l/T//fish.3gZf2K/fssh.fish @ line 2
 function fssh --description 'Fuzzy-find ssh host via ag and ssh into it'
-	rg --ignore-case '^host [^!*]' ~/.ssh/config* | cut -d ' ' -f 2 | fzf | read -l result; and ssh "$result"
+    if set -q argv[1]
+        rg --ignore-case '^host [^!*]' $argv[1] | cut -d ' ' -f 2 | sort | uniq | fzf | read -l result; and ssh "$result"
+    else
+        rg --ignore-case '^host [^!*]' ~/.ssh/config* | cut -d ' ' -f 2 | sort | uniq | fzf | read -l result; and ssh "$result"
+    end
 end
