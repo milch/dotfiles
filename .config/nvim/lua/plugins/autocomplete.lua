997e6b374819c349125034a0f6013afc37740f84
Manu Wallner <manu@supermil.ch>
2024-09-26 17:01:02 -0700

    Builtin neovim snippet expansion

--- .config/nvim/lua/plugins/autocomplete.lua
+++ .config/nvim/lua/plugins/autocomplete.lua
@@ -30,15 +30,7 @@ return {
 	"hrsh7th/nvim-cmp",
 	event = { "InsertEnter", "CmdlineEnter" },
 	dependencies = {
-		{
-			"L3MON4D3/LuaSnip",
-			dependencies = { "rafamadriz/friendly-snippets", "andys8/vscode-jest-snippets" },
-			config = function()
-				require("luasnip.loaders.from_vscode").lazy_load()
-			end,
-		},
 		"hrsh7th/cmp-path",
-		"saadparwaiz1/cmp_luasnip",
 		"hrsh7th/cmp-nvim-lsp",
 		"hrsh7th/cmp-cmdline",
 		"hrsh7th/cmp-buffer",
@@ -79,11 +71,6 @@ return {
 					return vim_item
 				end,
 			},
-			snippet = {
-				expand = function(args)
-					require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-				end,
-			},
 			window = {},
 			mapping = cmp.mapping.preset.insert({
 				["<C-b>"] = cmp.mapping.scroll_docs(-4),
@@ -101,37 +88,17 @@ return {
 						end
 					end,
 				}),
-				["<Tab>"] = cmp.mapping(function(fallback)
-					if cmp.visible() then
-						if #cmp.get_entries() == 1 then
-							cmp.confirm({ select = true })
-						else
-							cmp.select_next_item()
-						end
-					else
-						fallback()
-					end
-				end, { "i", "s" }),
-				["<S-Tab>"] = cmp.mapping.select_prev_item(),
 
-				-- Navigate between snippet placeholder
-				-- ["<C-n>"] = cmp_action.luasnip_jump_forward(),
-				-- ["<C-p>"] = cmp_action.luasnip_jump_backward(),
-				["<C-j>"] = {
-					i = cmp.mapping.select_next_item(),
-					c = cmp.mapping.select_next_item(),
-					s = cmp.mapping.select_next_item(),
-				},
-				["<C-k>"] = {
-					i = cmp.mapping.select_prev_item(),
-					c = cmp.mapping.select_prev_item(),
-					s = cmp.mapping.select_prev_item(),
-				},
+				["<C-j>"] = cmp.mapping(function()
+					cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+				end, { "i", "c", "s" }),
+				["<C-k>"] = cmp.mapping(function()
+					cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
+				end, { "i", "c", "s" }),
 				["<C-y>"] = cmp.mapping.confirm({ select = true }),
 			}),
 			sources = cmp.config.sources({
 				{ name = "nvim_lsp" },
-				{ name = "luasnip" }, -- For luasnip users.
 				{ name = "path" },
 			}),
 			experimental = {
---
+++ .config/nvim/lua/plugins/snippets.lua
@@ -0,0 +1,45 @@
+return {
+	{
+		"nvim-cmp",
+		dependencies = {
+			{
+				"garymjr/nvim-snippets",
+				opts = {
+					friendly_snippets = true,
+				},
+				dependencies = {
+					"rafamadriz/friendly-snippets",
+					"andys8/vscode-jest-snippets",
+				},
+			},
+		},
+		opts = function(_, opts)
+			opts.snippet = {
+				expand = function(args)
+					vim.snippet.expand(args.body)
+				end,
+			}
+			table.insert(opts.sources, { name = "snippets" })
+		end,
+		keys = {
+			{
+				"<C-n>",
+				function()
+					return vim.snippet.active({ direction = 1 }) and "<cmd>lua vim.snippet.jump(1)<cr>"
+				end,
+				expr = true,
+				silent = true,
+				mode = { "i", "s" },
+			},
+			{
+				"<C-p>",
+				function()
+					return vim.snippet.active({ direction = -1 }) and "<cmd>lua vim.snippet.jump(-1)<cr>"
+				end,
+				expr = true,
+				silent = true,
+				mode = { "i", "s" },
+			},
+		},
+	},
+}
