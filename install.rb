87058b547f45c314cb479fbb318b5b376124a6a4
Manu Wallner <manu@supermil.ch>
2024-09-27 22:34:02 -0700

    Slightly increase key repeat, kanata seems to be faster than kmonad

--- install.rb
+++ install.rb
@@ -124,7 +124,7 @@ def install_bat_theme
 end
 
 def set_key_repeat
-  `defaults write -g InitialKeyRepeat -int 10` # normal minimum is 15 (225 ms)
+  `defaults write -g InitialKeyRepeat -int 15` # normal minimum is 15 (225 ms)
   `defaults write -g KeyRepeat -int 1` # normal minimum is 2 (30 ms)`
 end
 
