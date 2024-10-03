bc6abc67ce03c569c9ea926182384e7d7a0cabb1
Manu Wallner <manu@supermil.ch>
2024-10-03 09:37:50 -0700

    inconsisent indentation

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -20,7 +20,7 @@ return {
 		-- stylua: ignore
 		keys = {
 			{ "<c-f>", function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end, silent = true, expr = true, desc = "Scroll Forward", mode = {"i", "n", "s"} },
-      { "<c-b>", function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true, expr = true, desc = "Scroll Backward", mode = {"i", "n", "s"}},
+			{ "<c-b>", function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true, expr = true, desc = "Scroll Backward", mode = {"i", "n", "s"}},
 		},
 		opts = {
 			views = {
