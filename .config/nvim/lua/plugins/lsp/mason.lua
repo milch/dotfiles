7aed3a2f767d2dbee4da160158dd08677860752a
Manu Wallner <manu@Auri.local>
2024-11-12 21:17:53 -0800

    Adjust mason keybind

--- .config/nvim/lua/plugins/lsp/mason.lua
+++ .config/nvim/lua/plugins/lsp/mason.lua
@@ -2,7 +2,7 @@ return {
 	{
 		"williamboman/mason.nvim",
 		cmd = "Mason",
-		keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
+		keys = { { "<leader>xm", "<cmd>Mason<cr>", desc = "Mason" } },
 		build = ":MasonUpdate",
 		opts_extend = { "ensure_installed" },
 		opts = { ensure_installed = { "shfmt" } },
