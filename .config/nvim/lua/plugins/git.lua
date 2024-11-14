3336b0edca11e661f603cb605aa87f767ff8de12
Manu Wallner <manu@Auri.local>
2024-11-13 22:24:14 -0800

    Add some gitsigns keybinds

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,41 +1,38 @@
 return {
-	"lewis6991/gitsigns.nvim",
-	event = { "LazyFile" },
-	opts = {
-		numhl = true,
+	{
+		"lewis6991/gitsigns.nvim",
+		event = { "LazyFile" },
+		opts = {
+			numhl = true,
+			on_attach = function(buffer)
+				local gs = package.loaded.gitsigns
+				local function map(l, r, desc, mode)
+					if mode == nil then
+						mode = "n"
+					end
+					vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
+				end
+				map("<leader>gs", gs.stage_hunk, "Stage hunk")
+				map("<leader>gS", gs.stage_buffer, "Stage buffer")
+				map("<leader>gu", gs.undo_stage_hunk, "Undo stage hunk")
+				map("<leader>gP", gs.preview_hunk, "Preview hunk")
+				map("<leader>gp", gs.preview_hunk_inline, "Preview hunk inline")
+				map("<leader>gx", gs.toggle_deleted, "Show deleted lines")
+
+				map("<leader>gr", ":Gitsigns reset_hunk<CR>", "Reset hunk", { "n", "v" })
+				map("<leader>gR", gs.reset_buffer, "Reset buffer")
+
+				-- stylua: ignore start
+				map("<leader>gb", function() gs.blame_line({ full = true }) end, "Blame Line")
+				map("<leader>gB", gs.blame, "Blame Buffer")
+				map("<leader>gd", gs.diffthis, "Diff This")
+				map("<leader>gD", function() gs.diffthis("~") end, "Diff This ~")
+				-- stylua: ignore end
+			end,
+		},
 	},
-	keys = {
-		{
-			"]h",
-			function()
-				if vim.wo.diff then
-					return "]c"
-				end
-				vim.schedule(function()
-					require("gitsigns").next_hunk()
-				end)
-				return "<Ignore>"
-			end,
-			{ expr = true, desc = "Go to next hunk" },
-		},
-		{
-			"[h",
-			function()
-				if vim.wo.diff then
-					return "[c"
-				end
-				vim.schedule(function()
-					require("gitsigns").prev_hunk()
-				end)
-				return "<Ignore>"
-			end,
-			{ expr = true, desc = "Go to previous hunk" },
-		},
-		-- TODO: Find prefix
-		-- { "s", ":Gitsigns stage_hunk<CR>", { silent = true } },
-		-- { "u", gitsigns.undo_stage_hunk },
-		-- { "S", gitsigns.stage_buffer },
-		-- { "p", gitsigns.preview_hunk },
-		-- { "x", gitsigns.toggle_deleted, { nowait = true } },
+	{
+		"sindrets/diffview.nvim",
+		opts = {},
 	},
 }
