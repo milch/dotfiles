d2586b6c6f89b17fcddc33f2ef9d95cf5e182926
Manu Wallner <manu@supermil.ch>
2025-04-20 16:23:32 -0700

    Migrate avante to new config

--- .config/nvim/lua/opt/ai/avante.lua
+++ .config/nvim/lua/opt/ai/avante.lua
@@ -5,33 +5,9 @@ return {
 		version = false, -- set this if you want to always pull the latest change
 		opts = {
 			provider = "ollama",
-			use_absolute_path = true,
-			vendors = {
-				---@type AvanteProvider
-				ollama = {
-					api_key_name = "",
-					endpoint = "http://localhost:11434/v1",
-					model = "deepseek-r1:32b",
-					parse_curl_args = function(opts, code_opts)
-						return {
-							url = opts.endpoint .. "/chat/completions",
-							headers = {
-								["Accept"] = "application/json",
-								["Content-Type"] = "application/json",
-								["x-api-key"] = "ollama",
-							},
-							body = {
-								model = opts.model,
-								messages = require("avante.providers").copilot.parse_messages(code_opts), -- you can make your own message, but this is very advanced
-								max_tokens = 2048 * 4,
-								stream = true,
-							},
-						}
-					end,
-					parse_response_data = function(data_stream, event_state, opts)
-						require("avante.providers").copilot.parse_response(data_stream, event_state, opts)
-					end,
-				},
+			ollama = {
+				endpoint = "http://localhost:11434",
+				model = "deepseek-r1:32b",
 			},
 		},
 		build = "make",
