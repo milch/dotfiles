b4a9fa6668e11a5aef48aeb2e2a5b0f5aa3034d5
Manu Wallner <wallman@amazon.com>
2025-05-21 13:05:15 -0700

    Formatting

--- .config/nvim/lua/plugins/ui/noice.lua
+++ .config/nvim/lua/plugins/ui/noice.lua
@@ -65,9 +65,9 @@ return {
 		},
 		-- stylua: ignore
 		keys = {
-			{ "<c-f>", function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end, silent = true, expr = true, desc = "Scroll Forward", mode = {"i", "n", "s"} },
-			{ "<c-b>", function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true, expr = true, desc = "Scroll Backward", mode = {"i", "n", "s"}},
-			{ "<leader>xn", "<cmd>Noice<CR>", desc = "Open Noice" },
+			{ "<c-f>",      function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end,  silent = true,      expr = true, desc = "Scroll Forward",  mode = { "i", "n", "s" } },
+			{ "<c-b>",      function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end, silent = true,      expr = true, desc = "Scroll Backward", mode = { "i", "n", "s" } },
+			{ "<leader>xn", "<cmd>Noice<CR>",                                                              desc = "Open Noice" },
 		},
 		opts = {
 			views = {
@@ -110,11 +110,11 @@ return {
 			},
 			-- you can enable a preset for easier configuration
 			presets = {
-				bottom_search = true, -- use a classic bottom cmdline for search
-				command_palette = true, -- position the cmdline and popupmenu together
+				bottom_search = true,     -- use a classic bottom cmdline for search
+				command_palette = true,   -- position the cmdline and popupmenu together
 				long_message_to_split = true, -- long messages will be sent to a split
-				inc_rename = false, -- enables an input dialog for inc-rename.nvim
-				lsp_doc_border = false, -- add a border to hover docs and signature help
+				inc_rename = false,       -- enables an input dialog for inc-rename.nvim
+				lsp_doc_border = false,   -- add a border to hover docs and signature help
 			},
 		},
 	},
