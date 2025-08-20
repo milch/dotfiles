555b730d28dee6b4631533749d7624183b46eff0
Manu Wallner <manu@supermil.ch>
2025-08-19 22:00:05 -0700

    The folke comment plugin

--- .config/nvim/lua/plugins/editor/comment.lua
+++ .config/nvim/lua/plugins/editor/comment.lua
@@ -1,15 +1,7 @@
 return {
 	{
-		"JoosepAlviste/nvim-ts-context-commentstring",
-		event = { "LazyFile", "VeryLazy" },
-		opts = { enable_autocmd = false },
-		init = function()
-			local get_option = vim.filetype.get_option
-			vim.filetype.get_option = function(filetype, option)
-				return option == "commentstring"
-						and require("ts_context_commentstring.internal").calculate_commentstring()
-					or get_option(filetype, option)
-			end
-		end,
+		"folke/ts-comments.nvim",
+		opts = {},
+		event = "LazyFile",
 	},
 }
