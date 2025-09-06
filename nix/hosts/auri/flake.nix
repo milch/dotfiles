c3e5de5c217d87edd5651da9e7863bb6c981397c
Manu Wallner <manu@supermil.ch>
2025-09-06 07:58:55 -0700

    Install garmin & wifiman on Auri

--- nix/hosts/auri/flake.nix
+++ nix/hosts/auri/flake.nix
@@ -51,6 +51,7 @@
     "discord"
     "expressvpn"
     "fastrawviewer"
+    "garmin-express"
     "google-chrome"
     "lm-studio"
     "mactex"
@@ -65,6 +66,7 @@
     "ultimaker-cura"
     "utm"
     "vlc"
+    "wifiman"
   ];
 
   system.defaults.dock = {
