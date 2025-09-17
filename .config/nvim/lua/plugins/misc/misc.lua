dd7047ba5a7cf6261211513b30c2c3e0ba101670
Manu Wallner <wallman@amazon.com>
2025-09-17 08:18:54 -0700

    Trying out Flash

--- .config/nvim/lua/plugins/misc/misc.lua
+++ .config/nvim/lua/plugins/misc/misc.lua
@@ -99,4 +99,33 @@ return {
 			{ "<leader>xz", "<cmd>ZenMode<cr>", desc = "Toggle Zen Mode" },
 		},
 	},
+	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		---@module 'flash'
+		---@type Flash.Config
+		opts = {
+			search = {
+				mode = "fuzzy",
+			},
+			jump = {
+				-- Automatically jump if there is only one match
+				autojump = true,
+			},
+			modes = {
+				-- Disable flash for fFtT movements
+				char = {
+					enabled = false,
+				},
+			},
+		},
+		-- stylua: ignore
+		keys = {
+			{ "s",     mode = { "n", "x", "o" }, function() require("flash").jump() end,              desc = "Flash" },
+			{ "S",     mode = { "n", "x", "o" }, function() require("flash").treesitter() end,        desc = "Flash Treesitter" },
+			{ "r",     mode = "o",               function() require("flash").remote() end,            desc = "Remote Flash" },
+			{ "R",     mode = { "o", "x" },      function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+			{ "<c-s>", mode = { "c" },           function() require("flash").toggle() end,            desc = "Toggle Flash Search" },
+		},
+	},
 }
