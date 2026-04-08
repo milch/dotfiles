5bda6a322a92b4f2af7c8816d571b39a7b25593c
Manu Wallner <manu@supermil.ch>
2026-04-07 17:40:40 -0700

    [nvim 0.12] vim.highlight.on_yank => vim.hl.on_yank

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -79,10 +79,7 @@ end
 vim.api.nvim_create_autocmd("TextYankPost", {
 	pattern = "*",
 	callback = function()
-		vim.highlight.on_yank({
-			higroup = "IncSearch",
-			timeout = 100,
-		})
+		vim.hl.on_yank({ timeout = 100 })
 	end,
 })
 
