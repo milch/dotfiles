4e5d629c1f5813e30ceaa8037098599f93b14e38
Manu Wallner <manu@supermil.ch>
2026-04-29 08:56:07 -0700

    Refactor keybindings + steer towards builtin bindings

--- .config/nvim/lua/keybindings.lua
+++ .config/nvim/lua/keybindings.lua
@@ -1,4 +1,12 @@
-local bind = vim.keymap.set
+---@param modes string|string[] Mode "short-name" (see |nvim_set_keymap()|), or a list thereof.
+---@param lhs string           Left-hand side |{lhs}| of the mapping.
+---@param rhs string|function  Right-hand side |{rhs}| of the mapping, can be a Lua function.
+---@param opts? vim.keymap.set.Opts|string
+local bind = function(modes, lhs, rhs, opts)
+	---@cast opts vim.keymap.set.Opts
+	opts = type(opts) == "string" and { desc = opts } or opts
+	vim.keymap.set(modes, lhs, rhs, opts)
+end
 
 local M = {}
 
@@ -12,19 +20,19 @@ function M.set()
 		end
 	end
 
-	bind("n", "<M-1>", goto_buffer(1), { desc = "Go directly to buffer 1" })
-	bind("n", "<M-2>", goto_buffer(2), { desc = "Go directly to buffer 2" })
-	bind("n", "<M-3>", goto_buffer(3), { desc = "Go directly to buffer 3" })
-	bind("n", "<M-4>", goto_buffer(4), { desc = "Go directly to buffer 4" })
-	bind("n", "<M-5>", goto_buffer(5), { desc = "Go directly to buffer 5" })
+	bind("n", "<M-1>", goto_buffer(1), "Go directly to buffer 1")
+	bind("n", "<M-2>", goto_buffer(2), "Go directly to buffer 2")
+	bind("n", "<M-3>", goto_buffer(3), "Go directly to buffer 3")
+	bind("n", "<M-4>", goto_buffer(4), "Go directly to buffer 4")
+	bind("n", "<M-5>", goto_buffer(5), "Go directly to buffer 5")
 
 	bind("v", "<C-j>", ":m '>+1<CR>gv=gv", { desc = "Move selected lines up", silent = true })
 	bind("v", "<C-k>", ":m '<-2<CR>gv=gv", { desc = "Move selected lines down", silent = true })
 
-	bind("n", "J", "mzJ`zmz", { desc = "Join lines (keep cursor position the same)" })
-	bind("n", "<C-d>", "<C-d>zz", { desc = "Move one page down (keep cursor centered)" })
-	bind("n", "<C-u>", "<C-u>zz", { desc = "Move one page up (keep cursor centered)" })
-	bind("n", "<C-o>", "<C-o>zz", { desc = "Move to previous location (keep cursor centered)" })
+	bind("n", "J", "mzJ`zmz", "Join lines (keep cursor position the same)")
+	bind("n", "<C-d>", "<C-d>zz", "Move one page down (keep cursor centered)")
+	bind("n", "<C-u>", "<C-u>zz", "Move one page up (keep cursor centered)")
+	bind("n", "<C-o>", "<C-o>zz", "Move to previous location (keep cursor centered)")
 
 	-- Scroll LSP hover / signature_help / diagnostic floats with <C-f>/<C-b>
 	-- without entering the float. Falls back to normal page scrolling when no
@@ -48,13 +56,13 @@ function M.set()
 	bind("n", "<C-b>", scroll_float("<C-u>"), { silent = true, desc = "Scroll float / page up" })
 
 	local sar = require("search_and_replace")
-	bind("n", "<Esc>", "<cmd>nohlsearch<cr>", { desc = "Clear search highlight" })
+	bind("n", "<Esc>", "<cmd>nohlsearch<cr>", "Clear search highlight")
 	bind({ "n", "x" }, "gs", sar.searchAndReplaceOperator, { expr = true, desc = "Search and replace operator" })
 	bind("n", "gs*", sar.searchAndReplaceCursorWord, { silent = true, desc = "Search and replace word under cursor" })
 
-	bind("v", "<leader>p", [["_dP]], { desc = "Replace the selection with the paste buffer, preserve the buffer" })
-	bind({ "n", "v" }, "<leader>y", [["+y]], { desc = "Copy to system clipboard" })
-	bind("n", "<leader>Y", [["+Y]], { desc = "Copy to system clipboard" })
+	bind("v", "<leader>p", [["_dP]], "Replace the selection with the paste buffer, preserve the buffer")
+	bind({ "n", "v" }, "<leader>y", [["+y]], "Copy to system clipboard")
+	bind("n", "<leader>Y", [["+Y]], "Copy to system clipboard")
 
 	bind("n", "<leader>w", [[<cmd>:w<CR>]], { desc = "Save", silent = true })
 
@@ -96,7 +104,7 @@ function M.set()
 		else
 			vim.cmd("copen")
 		end
-	end, { desc = "Toggle quickfix list" })
+	end, "Toggle quickfix list")
 
 	-- Removes the quickfix entry that the cursor is on with `dd`
 	vim.api.nvim_create_autocmd("FileType", {
@@ -119,58 +127,53 @@ function M.set()
 
 	bind("n", "<S-h>", function()
 		require("plugins.ui.lualine.harpoon-component").bprev()
-	end, { desc = "Prev Buffer" })
+	end, "Prev Buffer")
 	bind("n", "<S-l>", function()
 		require("plugins.ui.lualine.harpoon-component").bnext()
-	end, { desc = "Next Buffer" })
+	end, "Next Buffer")
 
-	bind("n", "<leader>bd", vim.cmd.bdelete, { desc = "Delete Buffer" })
+	bind("n", "<leader>bd", vim.cmd.bdelete, "Delete Buffer")
 
 	-- commenting
-	bind("n", "gco", "o<esc>Vcx<esc><cmd>normal gcc<cr>fxa<bs>", { desc = "Add Comment Below" })
-	bind("n", "gcO", "O<esc>Vcx<esc><cmd>normal gcc<cr>fxa<bs>", { desc = "Add Comment Above" })
+	bind("n", "gco", "o<esc>Vcx<esc><cmd>normal gcc<cr>fxa<bs>", "Add Comment Below")
+	bind("n", "gcO", "O<esc>Vcx<esc><cmd>normal gcc<cr>fxa<bs>", "Add Comment Above")
 
-	bind("i", "<M-BS>", "<C-w>", { desc = "Delete a whole word in insert mode with Alt+Backspace" })
+	bind("i", "<M-BS>", "<C-w>", "Delete a whole word in insert mode with Alt+Backspace")
 end
 
 function M.set_lsp(_, bufnr)
+	---@param built_in string
+	---@param modes string|string[] Mode "short-name" (see |nvim_set_keymap()|), or a list thereof.
+	---@param lhs string           Left-hand side |{lhs}| of the mapping.
+	---@param desc string
+	local steer = function(built_in, modes, lhs, desc)
+		local notif = function()
+			local msg = desc .. ": Use " .. built_in .. " instead"
+			Snacks.notifier.notify(msg, "info")
+		end
+		bind(modes, lhs, notif, { desc = desc })
+	end
 	local opts = { buf = bufnr, silent = true }
+
+	steer("gri", "n", "gi", "Go to implementation")
+	steer("gri", "n", "gI", "Goto Implementation")
+	steer("grn", "n", "<leader>cr", "Rename symbol")
+	steer("grr", "n", "gr", "Go to references")
+	steer("grt", "n", "gy", "Goto T[y]pe Definition")
+	steer("grx", { "v", "n" }, "<leader>cc", "Run Codelens")
+	steer("<C-s>", "i", "<c-k>", "Signature Help")
 
 	opts.desc = "Format code"
 	bind({ "n", "v", "x" }, "<leader>cf", require("conform").format, opts)
 
-	opts.desc = "Rename symbol"
-	bind("n", "<leader>cr", vim.lsp.buf.rename, opts)
-
 	opts.desc = "Go to definition"
 	bind("n", "gd", vim.lsp.buf.definition, opts)
-
-	opts.desc = "Go to references"
-	bind("n", "gr", vim.lsp.buf.references, opts)
-
-	opts.desc = "Show documentation for symbol under cursor"
-	bind("n", "K", vim.lsp.buf.hover, opts)
-
-	opts.desc = "Go to implementation"
-	bind("n", "gi", vim.lsp.buf.implementation, opts)
 
 	opts.desc = "Show code signature help"
 	bind("n", "gS", vim.lsp.buf.signature_help, opts)
 
-	opts.desc = "Signature Help"
-	bind("i", "<c-k>", vim.lsp.buf.signature_help, opts)
-
-	opts.desc = "Run Codelens"
-	bind({ "v", "n" }, "<leader>cc", vim.lsp.codelens.run, opts)
-
 	opts.desc = "Lsp Info"
 	bind("n", "<leader>cl", "<cmd>checkhealth vim.lsp<cr>", opts)
-
-	opts.desc = "Goto Implementation"
-	bind("n", "gI", vim.lsp.buf.implementation, opts)
-
-	opts.desc = "Goto T[y]pe Definition"
-	bind("n", "gy", vim.lsp.buf.type_definition, opts)
 
 	opts.desc = "Goto Declaration"
 	bind("n", "gD", vim.lsp.buf.declaration, opts)
