4fc9bf1ca00a1ff8dc527b9ab688412204f79cda
Manu Wallner <manu@supermil.ch>
2026-05-01 22:31:04 -0700

    Install comma (,)

--- flake.nix
+++ flake.nix
@@ -7,31 +7,46 @@
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nix-index-database.url = "github:nix-community/nix-index-database";
+    nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
-  let
-    mkDarwinSystem = { hostConfig, hostHomeModule ? ./nix/shared/home.nix }:
-      nix-darwin.lib.darwinSystem {
-        modules = [
-          hostConfig
-          home-manager.darwinModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.manu = hostHomeModule;
-          }
-        ];
+  outputs =
+    inputs@{
+      self,
+      nix-darwin,
+      nixpkgs,
+      home-manager,
+      nix-index-database,
+    }:
+    let
+      mkDarwinSystem =
+        {
+          hostConfig,
+          hostHomeModule ? ./nix/shared/home.nix,
+        }:
+        nix-darwin.lib.darwinSystem {
+          modules = [
+            hostConfig
+            home-manager.darwinModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.manu = hostHomeModule;
+            }
+            nix-index-database.darwinModules.nix-index
+            { programs.nix-index-database.comma.enable = true; }
+          ];
+        };
+    in
+    {
+      darwinConfigurations = {
+        "Auri" = mkDarwinSystem {
+          hostConfig = import ./nix/hosts/auri/flake.nix;
+          hostHomeModule = import ./nix/hosts/auri/home.nix;
+        };
       };
-  in
-  {
-    darwinConfigurations = {
-      "Auri" = mkDarwinSystem {
-        hostConfig = import ./nix/hosts/auri/flake.nix;
-        hostHomeModule = import ./nix/hosts/auri/home.nix;
-      };
+
+      darwinPackages = self.darwinConfigurations."Auri".pkgs;
     };
-
-    darwinPackages = self.darwinConfigurations."Auri".pkgs;
-  };
 }
